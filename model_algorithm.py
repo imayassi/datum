d312b3bf77532bb02d8b9bd7a735828dd60d9567
Mayassi <imayassi@corp.intuit.net>
2017-07-10 09:57:11 -0700

    Add learning curve code

--- model_algorithm.py
+++ model_algorithm.py
@@ -1,83 +1,26 @@
 
-import pandas as pd
-from sklearn.decomposition import PCA, NMF
-from binning import bin
 import numpy as np
-from statsmodels.tools import categorical
-import skfuzzy as fuzz
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import SelectFromModel
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+
 from sklearn.cross_validation import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.model_selection import ShuffleSplit
-from sklearn.preprocessing import Imputer, PolynomialFeatures
-from sklearn import datasets, cluster
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import make_moons, make_circles, make_classification
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier,VotingClassifier,RandomForestRegressor
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import roc_auc_score
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.cluster import KMeans
-from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, recall_score, roc_auc_score,  average_precision_score, f1_score
-from sklearn.cross_decomposition import PLSCanonical
-from sklearn import linear_model, decomposition, datasets
-from sklearn.ensemble import GradientBoostingClassifier, BaggingClassifier
-from binning import bin
-from sklearn.decomposition import PCA, NMF
-from sklearn.model_selection import StratifiedKFold
-from sklearn.feature_selection import RFECV
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.utils import check_random_state
 from sklearn.metrics import confusion_matrix
-from sklearn import preprocessing
-from sklearn.cross_decomposition import PLSRegression
-# import vertica_python
-import pyodbc
 import pandas as pd
-from pandas import Series,DataFrame
-# from retention_customer_type_features_new import customer_type_features
-from features_by_customer_type import customer_type_features
-from sklearn.feature_selection import SelectKBest, chi2
+from pandas import DataFrame
 import pickle
 random_state = np.random.RandomState(0)
 from sklearn.utils import shuffle
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.svm import SVR
 from sklearn import linear_model
-from sklearn.neural_network import MLPRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
 def algorithm(x,y, response):
     df=pd.concat([x,y], axis=1)
-    # label=list(df['LABELS'])
-    # for i in label:
-    #     print i
     y = df[response]
     x = df.drop(response, 1)
-    # x.drop(['LABELS'], axis=1, inplace=True)
     models = []
     x, y = shuffle(x, y, random_state=np.random.RandomState(0))
     y = y.astype(int)
-    poly = PolynomialFeatures(2)
-    # r=poly.fit_transform(x)
 
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.1, random_state=np.random.RandomState(0))
     C=1.0
